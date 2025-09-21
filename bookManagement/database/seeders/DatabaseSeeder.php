8e7fe030fc4cac09fd8a29e0e679f1e4a5d1b4ca
NgMinhTung664 <tunggzxje203@gmail.com>
2025-09-21 18:59:59 +0700

    update 1

--- bookManagement/database/seeders/DatabaseSeeder.php
+++ bookManagement/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,33 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User; 
+use App\Models\Book;   
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::updateOrCreate(
+            ['email' => 'test@example.com'], 
+            [
+                'name' => 'Test User',
+                'email_verified_at' => now(),
+                'password' => bcrypt('password'),
+                'remember_token' => Str::random(10),
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Book::create([
+            'bookName' => 'Avance Web',
+            'bookCode' => 'AW12398',
+            'bookAuthor' => 'Laravel',
+            'publishYear' => 2024,
+            'ISBN' => '123-456-789',
+            'category' => 'Programming',
+            'description' => 'Sách test cho project',
         ]);
     }
 }
