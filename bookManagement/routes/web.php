8b9f61b5c3079601ad751da5b56a9b39eea1098c
NgMinhTung664 <tunggzxje203@gmail.com>
2025-09-21 21:53:11 +0700

    update img

--- bookManagement/routes/web.php
+++ bookManagement/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
 
-Route::get('/book', [BookController::class, 'index']);
+Route::get('/home', function(){
+    return 'Welcome to the Page';
+});
